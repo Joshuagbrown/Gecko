58587798c2d469a5d64cec7188ba9dc45b9e1166
pwl24 <pwl24@uclive.ac.nz>
2022-08-02 14:01:45 +1200

    swan commit

--- src/main/java/seng202/team6/App.java
+++ src/main/java/seng202/team6/App.java
@@ -21,6 +21,7 @@ public class App {
         log.info("Hello World!");
         log.info("Josh is able to commit");
         log.info("Tara is able to commit");
+        log.info("swan is able to commit");
         log.warn("This is a warning message! Use this log type to 'warn' if something is not quite right");
         log.error("An error has occurred, thanks logging for helping find it! (This is a terrible error log message, but is only an example!')");
         log.log(Level.INFO, "There are many ways to log!");
