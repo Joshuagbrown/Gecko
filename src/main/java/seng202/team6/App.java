1c88a38b3c2210bc6a7d67c084882eaa5bebd7ec
TaraLipscombe <tli88@uclive.ac.nz>
2022-08-02 13:57:52 +1200

    testing how to commit

--- src/main/java/seng202/team6/App.java
+++ src/main/java/seng202/team6/App.java
@@ -20,6 +20,7 @@ public class App {
     public static void main(String[] args) {
         log.info("Hello World!");
         log.info("Josh is able to commit");
+        log.info("Tara is able to commit");
         log.warn("This is a warning message! Use this log type to 'warn' if something is not quite right");
         log.error("An error has occurred, thanks logging for helping find it! (This is a terrible error log message, but is only an example!')");
         log.log(Level.INFO, "There are many ways to log!");
