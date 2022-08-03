dc097e183f0e5189023065c31f3288d682ecd003
Joshua <jbr242@uclive.ac.nz>
2022-08-03 13:05:11 +1200

    added import assertEquals

--- src/test/java/seng202/team6/AppTest.java
+++ src/test/java/seng202/team6/AppTest.java
@@ -2,6 +2,7 @@ package seng202.team6;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
