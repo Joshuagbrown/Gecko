97930dddf5b63001ddcdce688834a0be8c4b8b27
pwl24 <pwl24@uclive.ac.nz>
2022-08-16 11:06:44 +1200

    Swan try to load the screen but fail

--- src/main/java/seng202/team6/controller/MapController.java
+++ src/main/java/seng202/team6/controller/MapController.java
@@ -1,4 +1,13 @@
 package seng202.team6.controller;
 
+import javafx.stage.Stage;
+
 public class MapController {
+
+    private Stage stage;
+    void init(Stage stage) {
+        this.stage = stage;
+    }
+
 }
+
