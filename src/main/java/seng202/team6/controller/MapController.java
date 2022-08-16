813a53462a5c9f2f532b92d5f2bba68f7c315c1b
pwl24 <pwl24@uclive.ac.nz>
2022-08-16 13:07:21 +1200

    alignment out

--- src/main/java/seng202/team6/controller/MapController.java
+++ src/main/java/seng202/team6/controller/MapController.java
@@ -3,11 +3,6 @@ package seng202.team6.controller;
 import javafx.stage.Stage;
 
 public class MapController {
-
     private Stage stage;
-    void init(Stage stage) {
-        this.stage = stage;
-    }
-
+    void init(Stage stage){this.stage = stage;}
 }
-
