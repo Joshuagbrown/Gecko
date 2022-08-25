4bc5a436866625043ca2354d5c4c2f3662d50c69
Philip Dolbel <pdo66@uclive.ac.nz>
2022-08-26 08:06:59 +1200

    fix checkstyle and add javadoc

--- src/main/java/seng202/team6/controller/MapToolBarController.java
+++ src/main/java/seng202/team6/controller/MapToolBarController.java
@@ -1,12 +1,19 @@
 package seng202.team6.controller;
 
-import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
+/**
+ * Controller for the map toolbar.
+ */
 public class MapToolBarController implements ToolBarController {
     private Stage stage;
     private MainScreenController controller;
 
+    /**
+     * Initializes the controller.
+     * @param stage Primary Stage of the application
+     * @param controller The Controller class for the main screen.
+     */
     @Override
     public void init(Stage stage, MainScreenController controller) {
         this.stage = stage;
--- src/main/java/seng202/team6/controller/ToolBarController.java
+++ src/main/java/seng202/team6/controller/ToolBarController.java
@@ -1,11 +1,16 @@
 package seng202.team6.controller;
 
-import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-import java.awt.*;
-import java.io.IOException;
-
-public interface ToolBarController  {
+/**
+ * Interface for all ToolBars in the app.
+ */
+public interface ToolBarController {
+    /**
+     * Initializes the Controller.
+     *
+     * @param stage Primary Stage of the application
+     * @param controller The Controller class for the main screen.
+     */
     void init(Stage stage, MainScreenController controller);
 }
