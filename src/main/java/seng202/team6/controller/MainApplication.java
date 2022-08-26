aac796971f7456f1daf54accc30a08f32453e913
Lucas Redding <lre61@uclive.ac.nz>
2022-08-26 12:48:40 +1200

    Added some not finalized colours to the main app pages

--- src/main/java/seng202/team6/controller/MainApplication.java
+++ src/main/java/seng202/team6/controller/MainApplication.java
@@ -28,6 +28,7 @@ public class MainApplication extends Application {
 
             primaryStage.setTitle("ElecTrip App");
             Scene scene = new Scene(root, 1200, 800);
+            scene.getStylesheets().add(getClass().getResource("/stylesheets/main.css").toExternalForm());
 
             // Add a custom application icon
 
