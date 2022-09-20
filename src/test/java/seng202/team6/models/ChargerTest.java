febb7358b6c44db9d34e2121d70fed856d1d3ec7
cma292 <cma292@uclive.ac.nz>
2022-09-21 09:50:27 +1200

    Junit for Charger.java

--- src/test/java/seng202/team6/models/ChargerTest.java
+++ src/test/java/seng202/team6/models/ChargerTest.java
@@ -8,6 +8,16 @@ class ChargerTest {
 
     @Test
     void testEquals() {
-
+        String plugType = "testPlug";
+        String plugType2 = "testPlug2";
+        String operative = "testOperative";
+        int wattage = 10;
+        Charger charger1 = new Charger(plugType, operative, wattage);
+        Charger charger2 = new Charger(plugType, operative, wattage);
+        Charger charger3 = new Charger(plugType2, operative, wattage);
+        boolean equals1 = charger1.equals(charger2);
+        boolean equals2 = charger1.equals(charger3);
+        assertEquals(true, equals1);
+        assertEquals(false, equals2);
     }
 }
